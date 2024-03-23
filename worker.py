df9f0ec531036c784fbee084032c3bbde8ee7070
JacobborstellCoder <117266139+JacobborstellCoder@users.noreply.github.com>
2024-03-23 17:06:44 +0100

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,6 +1,26 @@
 import random
-import scanner
 import time
+import json
+import asyncio
+import ipaddress
+import mcstatus
+
+
+
+running = True
+
+
+
+with open("config.json", "r") as file:
+    global CONFIG
+    CONFIG = json.load(file)
+
+TIMEOUT = CONFIG["TIMEOUT"]
+DEBUG = CONFIG["DEBUG"]
+PORTS = CONFIG["PORTS"]
+BATCH_SIZE = CONFIG["BATCH_SIZE"]
+USE_WEBHOOK = CONFIG["USE_WEBHOOK"]
+
 
 
 GREEN = "\033[1;32;48m"
@@ -9,8 +29,57 @@ RED = "\033[1;31;48m"
 END = "\033[1;37;0m"
 
 
-running = True
+if USE_WEBHOOK:
+    from discord_webhook import DiscordWebhook
+    with open("webhook.txt") as file:
+        WEBHOOK_URL = file.read().strip("\n")
 
+
+# Ips that you should proably not scan if you don't want legal trouble
+BAD_IPS = []
+with open("exclude.txt", "r") as file:
+    for line in file:
+        line = line.rstrip("\n")
+        line = line.split(" ")[0]
+        line
+        if len(line) == 0:
+            continue
+        if line[0] == "#":
+            continue
+        if "-" in line:
+            start_ip, end_ip = line.split('-')
+            start_ip, end_ip = ipaddress.IPv4Address(start_ip.strip()), ipaddress.IPv4Address(end_ip.strip())
+            BAD_IPS.extend(list(ipaddress.summarize_address_range(start_ip, end_ip)))
+        else:
+            BAD_IPS.append(ipaddress.IPv4Network(line))
+
+
+
+def is_bad_ip(ip):
+    try:
+        ip_obj = ipaddress.ip_address(ip)
+        for range in BAD_IPS:
+            if ip_obj in range:
+                return True
+    
+    except Exception:
+        return True
+    
+    return False
+
+
+
+def log_server(ip, port):
+    try:
+        with open("found_servers.txt", "a") as file:
+           file.write(ip + ":" + str(port) + "\n")
+
+    except Exception:
+       print(RED + "[WARN] An error occured while writing to the server list" + END)
+
+    finally:
+        if USE_WEBHOOK:
+            webhook = DiscordWebhook(url=WEBHOOK_URL, content="Found minecraft server: " + ip + ":" + str(port)).execute()
 
 
 def stop_threads():
@@ -22,33 +91,55 @@ def random_ip():
     return f"{random.randint(1, 255)}.{random.randint(1, 255)}.{random.randint(1, 255)}.{random.randint(1, 255)}"
 
 
-def log_server(ip, port):
-    try:
-        with open("found_servers.txt", "a") as file:
-           file.write(ip + ":" + str(port) + "\n")
-           print("[INFO] " + GREEN + "Successfully wrote the server " + ip + ":" + str(port) + " to the server list" + END)
-    except Exception:
-       print(RED + "[WARN] An error occured while writing to the server list" + END)
 
+
+
+
+
+def is_mcserver(ip, port):
+    try:
+        obj = mcstatus.JavaServer(host=ip, port=port, timeout=TIMEOUT * 2)
+        res = obj.status()
+        return True
+    except Exception as e:
+        return False
+
+
+async def scan_port(ip: str, port: int):
+    if DEBUG:
+        print("[INFO] Scanning:", ip + ":" + str(port))
+    
+    try:
+        r, w = await asyncio.wait_for(asyncio.open_connection(host=ip, port=port), TIMEOUT)
+        w.close()
+
+        if is_mcserver(ip, port):
+            print("[INFO] " + GREEN + "[SERVER] Found minecraft server: " + ip + ":" + str(port) + END)
+            log_server(ip, port)
+
+    except Exception:
+        pass
+
+
+async def scan_batch(size):
+    tasks = []
+
+    for i in range(round(size / len(PORTS))):
+        scan_ip = random_ip()
+        if is_bad_ip(scan_ip):
+            continue
+
+        for port in PORTS:
+            tasks.append(scan_port(scan_ip, port))
+        
+    await asyncio.gather(*tasks)
 
 
 def run_worker():
     time.sleep(1)
 
     while True:
-        scan_ip = random_ip()
-        if scanner.is_bad_ip(scan_ip):
-            continue
-        results = scanner.scan_ip(scan_ip)
-        if results:
-            for result in results:
-                ip, port, sock = result[0], result[1], result[2]
-                print("[INFO] " + YELLOW + "Found ip with port open: " + ip + ":" + str(port) + ", checking for minecraft servers..." + END)
-                res = scanner.is_mcserver(ip, port, sock)
-                if res:
-                    print("[INFO] " + GREEN + "[SERVER] Found minecraft server: " + ip + ":" + str(port) + END)
-                    log_server(ip=ip, port=port)
-                else:
-                    print("[INFO] " + YELLOW + "Did not find any minecraft server on: " + ip + ":" + str(port) + END)
+        asyncio.run(scan_batch(BATCH_SIZE))
+
         if not running:
             break
