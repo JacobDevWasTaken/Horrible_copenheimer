23728f3dbd125c64176c0bec73624fa509e743f8
JacobborstellCoder <117266139+JacobborstellCoder@users.noreply.github.com>
2024-03-23 17:06:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 import json
 
 
-VERSION = "v1.2.0"
+VERSION = "v1.3.0"
 
 
 with open("config.json", "r") as file:
@@ -12,12 +12,57 @@ with open("config.json", "r") as file:
     CONFIG = json.load(file)
 
 WORKERS = CONFIG["WORKERS"]
+BATCH_SIZE = CONFIG["BATCH_SIZE"]
 PORTS = CONFIG["PORTS"]
+TIMEOUT = CONFIG["TIMEOUT"]
+DEBUG = CONFIG["DEBUG"]
+USE_WEBHOOK = CONFIG["USE_WEBHOOK"]
+
+RATE = ((WORKERS * BATCH_SIZE) / len(PORTS)) / TIMEOUT
 
 
 
-print("[INFO] Starting Copenheimer " + VERSION + " with " + str(WORKERS) + " workers")
-print("[INFO] Starting workers...")
+
+print(f"""
+
+      
+
+
+
+     _____ ____  _____  ______ _   _ _    _ ______ _____ __  __ ______ _____  
+    / ____/ __ \|  __ \|  ____| \ | | |  | |  ____|_   _|  \/  |  ____|  __ \ 
+   | |   | |  | | |__) | |__  |  \| | |__| | |__    | | | \  / | |__  | |__) |
+   | |   | |  | |  ___/|  __| | . ` |  __  |  __|   | | | |\/| |  __| |  _  / 
+   | |___| |__| | |    | |____| |\  | |  | | |____ _| |_| |  | | |____| | \ \ 
+    \_____\____/|_|    |______|_| \_|_|  |_|______|_____|_|  |_|______|_|  \_\\
+
+
+
+Copenheimer {VERSION}
+https://github.com/JacobborstellCoder/Horrible_copenheimer
+                                                   
+""")
+
+
+
+
+
+print("[INFO] Starting Copenheimer " + VERSION + " with " + str(WORKERS) + " workers and a batch size of " + str(BATCH_SIZE))
+
+if DEBUG:
+    print("[INFO] Debug is enabled")
+
+if USE_WEBHOOK:
+    print("[INFO] Discord webhooks is enabled")
+
+print("[INFO] Scaning on ports: ", end="")
+for port in PORTS[:-1]:
+    print(port, end=", ")
+print(PORTS[-1], "with a timeout of " + str(TIMEOUT) + " seconds")
+
+print("[INFO] Estimated rate: " + str(RATE) + " IP's per second")
+print()
+print("[INFO] Starting " + str(WORKERS) +  " workers...", end=" ")
 
 
 workers_list = []
@@ -27,16 +72,9 @@ for i in range(WORKERS):
     workers_list.append(new_worker)
     new_worker.start()
 
-print("[INFO] Started " + str(WORKERS) + " workers")
-
-
-print("[INFO] Scaning on ports: ", end="")
-for port in PORTS[:-1]:
-    print(port, end=", ")
-print(PORTS[-1])
-
-print("[INFO] Scanning for servers...")
-print("[INFO] Press control + c to exit")
+print("done")
+print("[INFO] Scanning for servers, press control + c to exit")
+print()
 
 
 
@@ -47,7 +85,7 @@ try:
 except KeyboardInterrupt:
     print()
     print("[INFO] Shutting down Copenheimer...")
-    print("[INFO] Stopping workers...")
+    print("[INFO] Stopping workers... ")
 
     worker.stop_threads()
     for worker_to_stop in workers_list:
